08cc2de1da63076c8850bad04963fad859f1cc80
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-30 20:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",async()=>{
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+	if(!text || isNaN(delay)){
+		alert("please fill in all fields and provide a valid delay and text");
+		return;
+	}
+	await dealyFunction(delay);
+	document.getElementById("output").innerText = text;
+});
+function delayFunction(delay) {
+	return new Promise((resolve,reject)=>{
+		setTimeout(resolve,delay);
+	});
+	
+};
